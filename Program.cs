ee4034462270cb60d8d1f7423595fec4ee327574
Alexander <zam.kuratora@gmail.com>
2019-05-27 18:40:32 +0300

    Fix error;
    Fix error;

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using BinaryHelper;
+using System;
+using System.IO;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var br = new BinaryReader(File.OpenRead("player.bin"));
+
+            var offset = br.FindPosition(new byte[] { 0x6D, 0x6F, 0x6E, 0x65, 0x79 }, resetPosition: false); //money
+            if (offset)
+                Console.WriteLine($"Money: {br.ReadInt32()}");
+
+            offset = br.FindPosition(new byte[] { 0x68, 0x70 }, resetPosition: false); //hp
+            if (offset)
+                Console.WriteLine($"HP: {br.ReadByte()}");
+
+            br.Dispose();
+        }
+    }
+}
